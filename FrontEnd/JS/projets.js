ff0da7eab5f622fdf4ce4fa89cf278a92342a794
julef95 <lefevreju@hotmail.fr>
2023-03-03 12:37:18 +0100

    Ajout des filtres

--- FrontEnd/JS/projets.js
+++ FrontEnd/JS/projets.js
@@ -7,7 +7,7 @@ const valeurProjets = JSON.stringify(projets);
 console.log(valeurProjets)
 
 function afficherProjets(projets){
-    for (let i = 1; i < projets.length; i++) {
+    for (let i = 0; i < projets.length; i++) {
 
         // Récupération de l'élément du DOM qui accueillera les projets
         const galerie = document.querySelector(".gallery");
@@ -20,7 +20,7 @@ function afficherProjets(projets){
         const imageProjet = document.createElement("img");
         imageProjet.src = projets[i].imageUrl;
 
-        const nomProjet = document.createElement("h2");
+        const nomProjet = document.createElement("p");
         nomProjet.innerText = projets[i].title;
 
         // On rattache la balise projet a la section galerie
@@ -32,3 +32,47 @@ function afficherProjets(projets){
 }
 
 afficherProjets(projets);
+
+const boutonFiltreTous = document.querySelector("#boutonFiltreTous");
+
+boutonFiltreTous.addEventListener("click", function () {
+    const projetsFiltres = projets.filter(function (projets) {
+        return projets;
+    });
+    document.querySelector(".gallery").innerHTML = "";
+    afficherProjets(projetsFiltres);
+    console.log(projetsFiltres);
+});
+
+const boutonFiltreObjets = document.querySelector("#boutonFiltreObjets");
+
+boutonFiltreObjets.addEventListener("click", function () {
+    const projetsFiltres = projets.filter(function (projets) {
+        return projets.category.name == "Objets";
+    });
+    document.querySelector(".gallery").innerHTML = "";
+    afficherProjets(projetsFiltres);
+    console.log(projetsFiltres);
+});
+
+const boutonFiltreAppartements = document.querySelector("#boutonFiltreAppartements");
+
+boutonFiltreAppartements.addEventListener("click", function () {
+    const projetsFiltres = projets.filter(function (projets) {
+        return projets.category.name == "Appartements";
+    });
+    document.querySelector(".gallery").innerHTML = "";
+    afficherProjets(projetsFiltres);
+    console.log(projetsFiltres);
+});
+
+const boutonFiltreHotelsRestau = document.querySelector("#boutonFiltreHotelsRestau");
+
+boutonFiltreHotelsRestau.addEventListener("click", function () {
+    const projetsFiltres = projets.filter(function (projets) {
+        return projets.category.name == "Hotels & restaurants";
+    });
+    document.querySelector(".gallery").innerHTML = "";
+    afficherProjets(projetsFiltres);
+    console.log(projetsFiltres);
+});
